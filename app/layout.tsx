990dcc2da84bcd51185b455c3c13b798aee33d0e
themk1762010-alt <themk1762010@gmail.com>
2026-03-24 14:23:35 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,4 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "TREAZURE",
@@ -22,14 +10,12 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-return (
-  <html lang="en">
-    <head>
-      <link rel="icon" href="/icon.png" />
-    </head>
-    <body>{children}</body>
-  </html>
-);
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
